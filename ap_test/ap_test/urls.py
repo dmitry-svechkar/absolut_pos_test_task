9f75e66d1a0090821779ca66c96e3947414a4963
Дмитрий Свечкарь <svechkar.dmitry@yandex.ru>
2024-05-04 18:56:34 +0300

    added urls

--- ap_test/ap_test/urls.py
+++ ap_test/ap_test/urls.py
@@ -1,22 +1,13 @@
-"""
-URL configuration for ap_test project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from survey.views import survey_view
+
+from .views import FinishSurveyView, MainView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('survey/<int:question_id>/', survey_view, name='survey-view'),
+    path('survey/finish', FinishSurveyView.as_view(), name='finished_survey'),
+    path('', MainView.as_view(), name='main'),
 ]
